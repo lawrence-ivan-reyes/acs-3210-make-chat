caee8b11e5f78e17701b4c72d1f3dcc141eca48e
lawrence-ivan-reyes <lawrence.reyes@dominican.students.edu>
2025-09-22 16:24:15 -0400

    users can create new channels and join

--- sockets/chat.js
+++ sockets/chat.js
@@ -1,4 +1,5 @@
-module.exports = (io, socket, onlineUsers) => {
+//Make sure to add channels to module.exports parameters
+module.exports = (io, socket, onlineUsers, channels) => {
 
   // Listen for "new user" socket emits
   socket.on('new user', (username) => {
@@ -31,7 +32,17 @@ module.exports = (io, socket, onlineUsers) => {
   });
 
   socket.on('new channel', (newChannel) => {
-    console.log(newChannel);
+    //Save the new channel to our channels object. The array will hold the messages.
+    channels[newChannel] = [];
+    //Have the socket join the new channel room.
+    socket.join(newChannel);
+    //Inform all clients of the new channel.
+    io.emit('new channel', newChannel);
+    //Emit to the client that made the new channel, to change their channel to the one they made.
+    socket.emit('user changed channel', {
+      channel : newChannel,
+      messages : channels[newChannel]
+    });
   });
 
 }
